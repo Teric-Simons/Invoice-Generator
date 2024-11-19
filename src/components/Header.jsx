92f445fb777cccd77455987cf6943716769cd9a8
Unforturnacy <tericsimons12@gmail.com>
2024-11-19 17:58:36 -0500

    better formatting
    better formatting using prettier

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from "react";
 import "./css/header.css";
 
-export default function Header()
-{
-    return (
-        <header>
-            <p style={{fontSize:"23px", letterSpacing:"2px"}}>Teric's Invoice Generator</p>
-            <button>Github</button>
-        </header>
-    )
-
+export default function Header() {
+  return (
+    <header>
+      <p style={{ fontSize: "23px", letterSpacing: "2px" }}>
+        Teric's Invoice Generator
+      </p>
+      <button>Github</button>
+    </header>
+  );
 }
